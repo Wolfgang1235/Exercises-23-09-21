310ef5f83908ec6bd256ab839068e6ec673c7317
Wolfgang1235 <90389865+Wolfgang1235@users.noreply.github.com>
2021-09-23 16:14:37 +0200

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,13 @@
+class Student {
+	String name;
+	int age;
+	boolean isFemale;
+	int datamatikerTeam;
+
+	public Student(String tmpName, int tmpAge, boolean tmpIsFemale, int tmpDatamatikerTeam) {
+		name = tmpName;
+		age = tmpAge;
+		isFemale = tmpIsFemale;
+		datamatikerTeam = tmpDatamatikerTeam;
+	}
+}
